722c1fd9e004510662eb1f5e6ce26459ed2b4c5e
DB <80365717+nekoism@users.noreply.github.com>
2023-05-30 11:07:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,14 @@ const axios = require("axios");
 const cheerio = require('cheerio');
 const fs = require('fs');
 const client = require('https');
-//https://www.youtube.com/watch?v=9OlLxkaeVvw
-let name = prompt("Enter the name: ");
+
+let name = prompt("Enter the name: "); //gets the character name
 let url = `https://genshin-impact.fandom.com/wiki/${name}`
 getCharImage(url);
 let infoUrl = `https://api.genshin.dev/characters/${name.replace(" ","-")}`;
 getCharInfo(infoUrl)
-// const info
-// axios.get(url)
-//   .then(res =>{
-//     const $ = cheerio.load(res.data);
-//     $('article > .di-t').each((index,element) =>{
-//   	const title = $(element).find(".information > .title > a").text().replace("add","");
-//       const rating = $(element).find(".information > .pt8").text();
-//       	if(titile.length == 0 && rating.length == 0){
-//       		return;
-//       	}
-//   		console.log(title);
-//       	console.log(rating); //rating contain 
-//     })
-//   }).catch( err => console.log(err));
-//getting character image
+
+//functino which gets the character image 
 function getCharImage(url){
   axios.get(url)
 	.then(res => {
@@ -36,6 +23,7 @@ function getCharImage(url){
 
 }
 
+// function which gets the character infromation
 function getCharInfo(infoUrl){
   axios.get(infoUrl)
     .then(res => {
@@ -47,15 +35,3 @@ function getCharInfo(infoUrl){
       console.log(`Name: ${name} \n Vision: ${vision} \n Weapon Type: ${weapon} \n Character description: ${description}`)
     }).catch(err => console.log("Character Not Found"));
 }
-// axios.get(infoUrl)
-// Example
-/*
-const {data} = await axios.get('https://old.reddit.com/r/programming/');
-const $ = cheerio.load(data);
-const postTitles = [];
-
-$('div > p.title > a').each((_indx,el) =>{
-	const postTitle = $(el).text();
-	postTitles.push(postTitle);
-})
-*/
